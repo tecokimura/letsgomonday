976a68b3e56a68881ed82c2ad00d2ad5365fb3e7
tecokimura <tecokimura@gmail.com>
2017-11-24 18:10:12 +0900

    構造を検討中

---
+++ letsgomonday.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * グループから数名選んで抽選する。
+ * 結果を保存しておいてばらけて当選で
+ */
+main();
+exit;
+
+
+/**
+ * メイン処理
+ * @param array $argv
+ */
+function main(array $argv) {
+
+    // 起動引数を設定値に変換する
+
+    // 当日が祝日か調べる
+    $holiday = new Holiday();
+
+
+    // メンバーを取得する
+
+
+    // 前回までの抽選状況を読み込む
+
+    // 抽選する
+
+
+
+
+
+}
+
+
+/**
+ * 設定値を管理する
+ */
+class Config {
+
+}
+
+
+/**
+ * Class Holiday
+ */
+class Holiday {
+}
+
+
+/**
+ * Class Member
+ */
+class Member {
+}
+
+
+/**
+ * Class Lottery
+ */
+class Lottery {
+}
